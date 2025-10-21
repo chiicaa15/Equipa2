70c73de56ecd06b0fa4594bd7bd8ec41692d38cd
chiicaa15 <joaomarques0243@gmail.com>
2025-10-21 18:02:18 +0100

    criação da classe student e volunteermanager

---
+++ projeto-voluntariado/src/main/java/Voluntariado/Student.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Voluntariado;
+
+/**
+ * 
+ */
+public class Student extends User {
+	private int num;
+
+	/**
+	 * @param nome
+	 * @param email
+	 * @param password
+	 * @param num
+	 */
+	public Student(String nome, String email, String password, int num) {
+		super(nome, email, password);
+		this.num = num;
+	}
+
+	/**
+	 * @return the num
+	 */
+	public int getNum() {
+		return num;
+	}
+	
+	
+
+}
---
+++ projeto-voluntariado/src/main/java/Voluntariado/VolunteerManager.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Voluntariado;
+
+/**
+ * 
+ */
+public class VolunteerManager extends User {
+
+	/**
+	 * @param nome
+	 * @param email
+	 * @param password
+	 */
+	public VolunteerManager(String nome, String email, String password) {
+		super(nome, email, password);
+	}
+	
+	
+
+}
