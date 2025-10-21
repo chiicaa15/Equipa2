642500cd0fff7fa19a0682fdd9b62a487469774a
chiicaa15 <joaomarques0243@gmail.com>
2025-10-21 17:51:14 +0100

    ficheiro pom e adição dos atributos e construtor da classe User

--- projeto-voluntariado/src/main/java/Voluntariado/User.java
+++ projeto-voluntariado/src/main/java/Voluntariado/User.java
@@ -7,5 +7,20 @@ package Voluntariado;
  * 
  */
 public class User {
-
+	private String nome;
+	private String email;
+	private String password;
+	/**
+	 * @param nome
+	 * @param email
+	 * @param password
+	 */
+	public User(String nome, String email, String password) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
 }
