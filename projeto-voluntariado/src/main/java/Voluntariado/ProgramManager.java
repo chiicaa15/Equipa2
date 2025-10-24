1ac77c64ab855d32f80c042ef5ddd86bf14ef3eb
Beatriz Pimentel <Beatriz Pimentel@Lua>
2025-10-24 09:38:57 +0100

    Create Arrays class ProgramManager

--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -3,9 +3,25 @@
  */
 package Voluntariado;
 
+import java.util.ArrayList;
+
 /**
  * 
  */
 public class ProgramManager {
+	
+	private ArrayList <Program> programs;
+	private ArrayList <Student> students;
+	private ArrayList <User> users;
+	
+	
+	public ProgramManager(ArrayList<Program> programs, ArrayList<Student> students, ArrayList<User> users) {
+		super();
+		
+	}
+	
+	
+	
+	
 	
 }
