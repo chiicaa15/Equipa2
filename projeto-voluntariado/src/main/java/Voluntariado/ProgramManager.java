5826fbd281e5112a6506f49dce7463c46b684191
chiicaa15 <joaomarques0243@gmail.com>
2025-10-24 09:12:22 +0100

    atualização da classe main e program manager

--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -2,10 +2,48 @@
  * 
  */
 package Voluntariado;
-
+import org.hibernate.Session; 
+import org.hibernate.SessionFactory; 
+import org.hibernate.boot.MetadataSources; 
+import org.hibernate.boot.registry.StandardServiceRegistry; 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 /**
  * 
  */
 public class ProgramManager {
+	public SessionFactory sessionFactory;
 	
+	public void setup() {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+			.configure()
+			.build();
+		try {
+			sessionFactory= new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception ex) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+	
+	public void exit() {
+		
+	}
+	
+	public void create() {
+		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		
+		
+	}
+	public void read() {
+		
+	}
+	public void update() {
+		
+	}
+	public void delete() {
+		
+	}
 }
+
