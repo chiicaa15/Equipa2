6557991f071850a56183fe32546722180e2988ce
Beatriz Pimentel <Beatriz Pimentel@Lua>
2025-10-25 23:22:03 +0100

    criação do método criarPrograma

--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -5,14 +5,17 @@ package Voluntariado;
 
 import java.util.ArrayList;
 
+import org.hibernate.Session;
+
+import jakarta.transaction.Transaction;
+
+
 /**
  * 
  */
 public class ProgramManager {
 	
 	private ArrayList <Program> programs;
-	private ArrayList <Student> students;
-	private ArrayList <User> users;
 	
 	
 	public ProgramManager(ArrayList<Program> programs, ArrayList<Student> students, ArrayList<User> users) {
@@ -20,8 +23,39 @@ public class ProgramManager {
 		
 	}
 	
+	/*
+	 * public boolean login (String nome, String email, String password ) {
+	 * 
+	 * 
+	 * return false;
+	 * 
+	 * }
+	 */
 	
 	
-	
+	public void criarPrograma (String nomeP, String description, String location, int contact) {
+		
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSessions()){
+			tx = session.beginTransaction();
+			
+			Program p = new Program (nomeP, description, location, contact);
+			session.persist(p);
+			
+			tx.commit();
+			
+			System.out.println("O programa foi criado com sucesso!");
+		}
+		
+		catch (Exception e) {
+			if (tx = null) tx.rollback();
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
 	
 }
