37ab1ce121cc3268b9f592cc744492e7cab6a5c8
Beatriz Pimentel <Beatriz Pimentel@Lua>
2025-10-26 14:51:40 +0000

    Main - criação do main para o método criarPrograma

--- projeto-voluntariado/src/main/java/Voluntariado/Main.java
+++ projeto-voluntariado/src/main/java/Voluntariado/Main.java
@@ -9,6 +9,29 @@ import org.hibernate.Session;
 public class Main {
 	public static void main(String[]args) {
 		
+	
+	case 1:
+		System.out.println("Insira o nome do programa: ");
+		String nomeP = input.nextLine();
+		
+		System.out.println("Insira uma descrição: ");
+		String description = input.nextLine();
+		
+		System.out.println("Insira a localização do programa: ");
+		String location = input.nextLine();
+		
+		System.out.println("Insira o contacto responsável: ");
+		int contact = input.nextLine();
+		
+		System.out.println("Insira a que tipo o programa pertence: ");
+		String type = input.nextLine();
+		
+		//chamar o método
+		Program p = new Program(nomeP, description, location, contact, type);
+		manager.criarPrograma(p);
+		
 	}
+	
+	
 	
 }
--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -80,9 +80,6 @@ public class ProgramManager {
 		}
 		
 		
-		
-		
-		
 	}
 	
 }
