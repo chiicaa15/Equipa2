3cbb38a9075ef0796005506af293c5c0919378eb
Beatriz Antunes <biasoaresantunes15@gmail.com>
2025-10-26 17:23:36 +0000

    adicionei o query

--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -88,7 +88,7 @@ public class ProgramManager {
 	
 	public List<Program> searchProgram(String type, String Partner) {
 	 try (Session session = sessionFactory.openSession()) {
-	        
+		 Query<Program> query;
 	}
 	
 	public void printProgram() {
