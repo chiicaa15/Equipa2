63ee3728f5b3f5c93fd0a5b00d953805c196126b
Beatriz Pimentel <Beatriz Pimentel@Lua>
2025-10-26 14:22:49 +0000

    Método criarPrograma

--- projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
+++ projeto-voluntariado/src/main/java/Voluntariado/ProgramManager.java
@@ -16,6 +16,7 @@ import jakarta.transaction.Transaction;
 public class ProgramManager {
 	
 	private ArrayList <Program> programs;
+	private ArrayList <Type> tipos;
 	
 	
 	public ProgramManager(ArrayList<Program> programs, ArrayList<Student> students, ArrayList<User> users) {
@@ -33,14 +34,36 @@ public class ProgramManager {
 	 */
 	
 	
-	public void criarPrograma (String nomeP, String description, String location, int contact) {
+	
+	public void criarPrograma (String nomeP, String description, String location, int contact, String type) {
 		
-		Transaction tx = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSessions()){
-			tx = session.beginTransaction();
+		//verificar se está vazio ou não
+		if (nomeP.isEmpty() || description.isEmpty() || location.isEmpty() || contact.isEmpty || type.isEmpty()) {
+			System.out.println("Todos os campos têm de ser preenchidos!");
+			return;
+		}
 			
-			Program p = new Program (nomeP, description, location, contact);
+		
+		Transaction tx = null; //tx guarda a transação
+		try (Session session = HibernateUtil.getSessionFactory().openSessions()){ //abre uma nova sessão com b.dados e guarda em session
+			tx = session.beginTransaction(); //começa
+			
+			
+			//verificar se o tipo existe, se não cria
+			Query<Type> query = session.createQuery("from Type where type = :type", Type.class);
+			query.setParameter("type", typeType);
+			Type type = query.uniqueResult();
+			
+			if (type == null) {
+				type = new Type(typeType); //typeType pra usar o valor
+				session.persist(type);
+			}
+			
+			
+			//criação do programa e adicionar o programa a um dado type
+			Program p = new Program (nomeP, description, location, contact, type);
 			session.persist(p);
+			type.getProgramas().add(p);
 			
 			tx.commit();
 			
@@ -48,8 +71,12 @@ public class ProgramManager {
 		}
 		
 		catch (Exception e) {
-			if (tx = null) tx.rollback();
+			
+			
+			//se algo deu errado, nada irá ser salvo
+			if (tx = null) tx.rollback(); 
 			e.printStackTrace();
+			System.out.println("Programa não criado por: " + e.getMessage()); //mostra os motivos de não ter sido criado
 		}
 		
 		
